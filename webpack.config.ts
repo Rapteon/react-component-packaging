3e94e903609523109d7a20c14e16b31e67c49581
Rapteon <rapteon@tutanota.com>
2026-07-09 09:39:41 +0530

    chore:clean output folder each time.

--- webpack.config.ts
+++ webpack.config.ts
@@ -37,6 +37,7 @@ const config: webpack.Configuration = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
   devtool: "inline-source-map"
 };
