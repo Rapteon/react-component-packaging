06a8df6b51b2ec200724498ce752632111fdd775
Rapteon <rapteon@tutanota.com>
2026-07-09 11:53:42 +0530

    chore:Refactored webpack config to use single config file and fixed issue with prod builds not being importable.

--- webpack.config.ts
+++ webpack.config.ts
@@ -5,50 +5,51 @@ import webpack from "webpack";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const config: webpack.Configuration = {
-  mode: "development",
-  entry: "./src/index.ts",
-  module: {
-    rules: [
-      {
-        test: /\.([cm]?ts|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-            loader: "babel-loader",
-            options: {
-                presets: [
-                    '@babel/preset-env',
-                    '@babel/preset-react',
-                    '@babel/preset-typescript'
-                ]
-            }
+export default (env, argv) => {
+  const config: webpack.Configuration = {
+    mode: "none",
+    entry: "./src/index.ts",
+    module: {
+      rules: [
+        {
+          test: /\.([cm]?ts|tsx)$/,
+          exclude: /node_modules/,
+          use: {
+              loader: "babel-loader",
+              options: {
+                  presets: [
+                      '@babel/preset-env',
+                      ['@babel/preset-react', {runtime: "automatic", development: argv.mode !== "production"}],
+                      '@babel/preset-typescript'
+                  ]
+              }
+          }
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
         }
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
+      ],
+    },
+    resolve: {
+      fullySpecified: true,
+      extensions: [".tsx", ".ts", ".js"],
+      extensionAlias: {
+          ".js": [".js", ".ts"],
+          ".cjs": [".cjs", ".cts"],
+          ".mjs": [".mjs", ".mts"]
       }
-    ],
-  },
-  resolve: {
-    fullySpecified: true,
-    extensions: [".tsx", ".ts", ".js"],
-    extensionAlias: {
-        ".js": [".js", ".ts"],
-        ".cjs": [".cjs", ".cts"],
-        ".mjs": [".mjs", ".mts"]
-    }
-  },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-    library: {
-        name: "myComponents",
-        type: "umd"
-    }
-  },
-  devtool: "inline-source-map"
-};
-
-export default config;
+    },
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+      library: {
+          name: "myComponents",
+          type: "umd"
+      }
+    },
+    devtool: argv.mode === "production"? "source-map" : "inline-source-map"
+  };
+  return config;
+}
